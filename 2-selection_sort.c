fc3595dcfd70f17d9721274750526f28c9ed12d2
osetamara <osemudiamebrifini@gmail.com>
2023-09-25 02:22:57 -0700

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,47 +1,39 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-/**
- * selection_sort - Sort an array of integers in ascending order
- *                  using the selection sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
- *
- * Description: Prints the array after each swap.
+ * selection_sort - Sorts an array using the selection sort algorithm.
+ * @array: The array of integers to be sorted.
+ * @size: The number of elements in the array.
  */
 void selection_sort(int *array, size_t size)
 {
-	int *min; /*Pointer to minimum element in unsorted part of the array.*/
-	size_t r, k;
-	/*Check if the array is NULL or has less than 2 elements.*/
-	if (array == NULL || size < 2)
-		return;
-	/*Loop through the array to select the minimum element in each pass.*/
-	for (r = 0; r < size - 1; r++)
-	{
-		min = array + i;/*Assume the current element is the minimum.*/
-		/*Find the actual minimum element in the unsorted part of the array.*/
-		for (k = r + 1; k < size; k++)
-			min = (array[k] < *min) ? (array + k) : min;
-		/*If the assumed minimum is not the actual minimum, swap them.*/
+	size_t r, d, less, tmp, swap;
 
-		if ((array + r) != min)
+	if (array == NULL)
+		return;/*If the array is empty, nothing to sort, so return.*/
+
+	for (r = 0; r < size; r++)
+	{
+		/*Initialize variables to keep track of index of smallest element*/
+		/*and whether a swap has occurred.*/
+		for (less = r, d = r; d < size; d++)
 		{
-			swap_ints(array + r, min);
+			/*Find the index of the smallest element in the unsorted part of the array.*/
+			if (array[d] < array[less])
+			{
+				less = d; /*Update the index of the smallest element.*/
+				swap = 1;/*Set swap to 1 to indicate that a swap is needed.*/
+			}
+		}
+		if (swap == 1)
+		{
+			/*Swap the smallest element with the current element.*/
+			tmp = array[less];
+			array[less] = array[r];
+			array[r] = tmp;
 			/*Print the array after each swap.*/
 			print_array(array, size);
+			swap = 0;/*Reset swap to 0 for the next iteration.*/
 		}
 	}
 }
